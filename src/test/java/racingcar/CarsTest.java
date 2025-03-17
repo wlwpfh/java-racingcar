e9d335dbe7ad76ee3582f48e9a4c0f53a92b6ec7
yang Jiyoung <0112jyoung@naver.com>
2025-03-17 21:49:56 +0900

    [refactor] 테스트코드 수정

--- src/test/java/racingcar/CarsTest.java
+++ src/test/java/racingcar/CarsTest.java
@@ -1,16 +1,24 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarsTest {
+    private Cars cars;
+    @BeforeEach
+    void setUp(){
+        cars = new Cars(3);
+    }
 
-    @ParameterizedTest
-    @ValueSource(ints = {4})
-    void initializeCars(int number) {
-        assertThat(Cars.initializeCars(number))
-                .hasSize(number);
+    @Test
+    @DisplayName("생성된 자동차의 개수를 확인한다.")
+    void initialize(){
+        assertEquals(3, cars.getCars().size());
     }
 }
